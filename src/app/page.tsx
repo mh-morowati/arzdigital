ca1dfe8a09280a534609bc1aacc45f42ef654faa
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-10-29 16:10:42 +0330

    test liara

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Prices from '../components/price/index';
  const Home = () => {
   return (
     <div>
-      <div className=" md:mx-auto mt-14 mb-14 md:w-[70%]">
+      <div className=" md:mx-auto mt-14 mb-14 md:w-[70%] bg-yellow-500">
         <h1 className="text-center md:text-3xl text-[#30505c]">قیمت لحظه‌ ای ارز‌های دیجیتال</h1>
         <div className='mt-10'>
        <Prices/>
