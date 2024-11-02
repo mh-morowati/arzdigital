909c2dccec40e763cc0711efc416766865b65cc4
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-11-02 14:02:08 +0330

    add tether price
    receive tether price with axios from api and show in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { TbBraille } from "react-icons/tb";
+import { TbTransactionBitcoin } from "react-icons/tb";
 import Search from './search';
+import TetherPrice from './tetherPrice';
 
 
 function Header() {
@@ -9,7 +11,9 @@ function Header() {
             <span className="text-2xl tracking-tighter align-middle font-medium text-[#1e2025]">ArzDigital</span></Link>
             <Search/>
             <div className="float-right">
-            <Link href={"/coins"}><h4>ارزها: <span className="text-[#1aa089]">100</span></h4>
+            <h1 className="inline-block">قیمت تتر: <TetherPrice/> تومان</h1>
+                <button className="hover:bg-slate-200 rounded py-1 px-3"><TbTransactionBitcoin className="inline-block w-5 h-5 text-red-400"/>ماشین حساب ارز دیجیتال</button>
+            <Link href={"/coins"}><button className="hover:bg-slate-200 rounded py-1 px-3">ارزها: <span className="text-[#1aa089]">100</span></button>
             </Link>
             </div>
     </div>);
---
+++ src/components/header/tetherPrice.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+interface OrderbookResponse {
+    lastTradePrice: string;
+}
+
+async function TetherPrice() {
+
+    const response = await axios.get<OrderbookResponse>('https://api.nobitex.ir/v3/orderbook/USDTIRT');
+
+    return (<>
+        {response.data.lastTradePrice}
+    </>);
+}
+
+export default TetherPrice;
