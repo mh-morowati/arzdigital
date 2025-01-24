f4f204866c762b11d78d3571022f4648c7f5b8e1
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-01-24 05:29:04 -1200

    add catch api currency selected with nameId

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,10 +1,16 @@
+import axios from "axios"
 
 const getCurrencyList = async () => {
-    const response = await fetch('https://api.coinlore.net/api/tickers/')
-    return response
-  }
+  const response = await fetch('https://api.coinlore.net/api/tickers/')
+  return response
+}
 
-  export const apiService = {
-    getCurrencyList,
+const getCurrencyFocus = async (nameId: string) => {
+  const response = await axios.get(`https://api.coincap.io/v2/assets/${nameId}`)
+  return response
+}
 
-  }
+export const apiService = {
+  getCurrencyList,
+  getCurrencyFocus
+}
--- src/app/coins/[id]/page.tsx
+++ src/app/coins/[id]/page.tsx
@@ -1,7 +1,32 @@
+"use client"
+import { apiService } from "@/api/services";
+import { useEffect } from "react";
 
+interface ProductProps {
+    params: {
+      id?: string;
+    };
+  }
 
-function Currency() {
-    return ( <p>currency page</p> );
+const Currency: React.FC<ProductProps> = ( {params}) => {
+    const id = params.id || ''
+
+    useEffect(() => {
+    // get countries from server
+    (
+      async () => {
+        try {
+          const response = await apiService.getCurrencyFocus(id)
+          console.log(response.data)
+        }
+        catch (err) {
+          console.error(err)
+        }
+      }
+    )()
+  }, [id])
+
+    return ( <h1>{id}</h1> );
 }
 
 export default Currency;
